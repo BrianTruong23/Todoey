545a5aa513ba4155960206ec077a77f212b0ed7f
Brian2003 <truongthoithang@gmail.com>
2018-07-02 09:27:38 -0500

    Design the structue MVC

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  item.swift
+//  Todoey
+//
+//  Created by MACBOOK on 7/2/18.
+//  Copyright © 2018 Brian Truong. All rights reserved.
+//
+
+import UIKit
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
